a114f3032f374bcdda67bbfc979ffa4edb134147
Stay <stay@zaroz.es>
2023-06-09 19:53:26 +0200

    AdminLayout

---
+++ client/src/layouts/AdminLayout.tsx
@@ -0,0 +1,27 @@
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router"
+import { IdentityContext } from "../context/IdentityContext";
+
+const AdminLayout = () =>  {
+    const identity = useContext(IdentityContext)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (identity.currentUser == undefined) {
+            navigate("/login")
+            return;
+        }
+
+        if (identity.currentUser != undefined && !identity.currentUser.admin) {
+            navigate("/")
+        }
+    }, [identity.currentUser, navigate])
+
+    if (identity.currentUser != undefined && !identity.currentUser.admin) return (<label>redirecting</label>)
+    
+    return (
+        <Outlet/>
+    )
+}
+
+export default AdminLayout;
