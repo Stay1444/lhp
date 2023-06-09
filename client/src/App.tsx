6c445604606529ca511857f0b127cbce361e2e3e
Stay <stay@zaroz.es>
2023-06-09 19:54:08 +0200

    Added some pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,31 +3,40 @@ import './app.sass'
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/user/HomePage';
 import { LanguageContextProvider } from './context/LanguageContext';
+import LoggedOutLayout from './layouts/LoggedOutLayout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { IdentityContextProvider } from './context/IdentityContext';
+import AdminLayout from './layouts/AdminLayout';
 
 const App = () => {
   return (
     <LanguageContextProvider>
-      <Routes>
-        <Route path='/login' element={<h1>LOGIN</h1>}/>
-        <Route path='/register' element={<h1>REGISTER</h1>}/>
-
-        <Route path='/' element={<MainLayout/>}>
-          <Route index element={<HomePage/>}/>
-          <Route path='machines' element={<h1>MACHINES</h1>}/>
-          <Route path='domains' element={<h1>DOMAINS</h1>}/>
-
-          <Route path='admin'>
-            <Route index element={<h1>ADMIN</h1>}/>
-            <Route path='users' element={<h1>ADMIN USERS</h1>}/>
-            <Route path='domains' element={<h1>ADMIN DOMAINS</h1>}/>
-            <Route path='machines' element={<h1>ADMIN MACHINES</h1>}/>
-            <Route path='addresses' element={<h1>ADDRESSES</h1>}/>
+      <IdentityContextProvider>        
+        <Routes>
+          <Route element={<LoggedOutLayout/>}>
+            <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/register' element={<RegisterPage/>}/>
           </Route>
-          
-        </Route>
 
-        <Route path='*' element={<Navigate to='/'/>}/>
-      </Routes>
+          <Route path='/' element={<MainLayout/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path='machines' element={<h1>MACHINES</h1>}/>
+            <Route path='domains' element={<h1>DOMAINS</h1>}/>
+
+            <Route path='admin' element={<AdminLayout/>}>
+              <Route index element={<h1>ADMIN</h1>}/>
+              <Route path='users' element={<h1>ADMIN USERS</h1>}/>
+              <Route path='domains' element={<h1>ADMIN DOMAINS</h1>}/>
+              <Route path='machines' element={<h1>ADMIN MACHINES</h1>}/>
+              <Route path='addresses' element={<h1>ADDRESSES</h1>}/>
+            </Route>
+            
+          </Route>
+
+          <Route path='*' element={<Navigate to='/'/>}/>
+        </Routes>
+      </IdentityContextProvider>
     </LanguageContextProvider>
   )
 }
