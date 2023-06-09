719e328e12a5a2c92bbedd7b29ce1fcbaf725563
Stay <stay@zaroz.es>
2023-06-09 18:41:59 +0200

    Created User

--- client/src/api/User.tsx
+++ client/src/api/User.tsx
@@ -1,3 +1,8 @@
 export default class User {
-    
+    public Id!: string
+    public Token!: string
+    public Admin!: boolean
+    public CreationDate!: Date
+    public LastLoginDate!: Date
+    public Name!: string
 }
