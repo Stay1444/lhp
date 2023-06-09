2b5bca58fe4f65d6f44ceecad68daf9daac7cd13
Stay <stay@zaroz.es>
2023-06-09 18:41:54 +0200

    Added onSubmit

--- client/src/components/TextField.tsx
+++ client/src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { RefObject, useEffect, useRef } from "react";
 
 import style from "./TextField.module.sass"
 
@@ -7,13 +7,31 @@ type TextFieldProps = {
     onChange?: (text: string) => void
     className?: string,
     type?: "text" | "number",
-    value?: string
+    value?: string,
+    onSubmit?: () => void
 };
 
 
-const TextField: React.FC<TextFieldProps> = ({type = "text", value = "", placeholder, onChange, className}) => {
+const TextField: React.FC<TextFieldProps> = ({type = "text", value = "", placeholder, className, onChange, onSubmit}) => {
+    const element = useRef<HTMLInputElement>(null);
+    
+    useEffect(() => {
+
+        const handle = (e: KeyboardEvent) => {
+            if (e.code == "Enter" && onSubmit != null && element.current == document.activeElement) {
+                onSubmit()
+            } 
+        }
+
+        document.addEventListener("keypress", handle);
+
+        return () => {
+            document.removeEventListener("keypress", handle);
+        }
+    }, [element, onSubmit])
+
     return (
-        <input type={type} placeholder={placeholder} value={value} className={`${style.input} ${className}`} onChange={(e) => {
+        <input ref={element} type={type} placeholder={placeholder} value={value} className={`${style.input} ${className}`} onChange={(e) => {
             if (onChange == null) return;
             onChange(e.target.value);
         }}/>
