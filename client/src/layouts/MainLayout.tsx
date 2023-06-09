3fef279d66a34a4cd0b639125c1289d241389fbd
Stay <stay@zaroz.es>
2023-06-09 15:42:53 +0200

    Require authentication

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,10 +1,22 @@
 import style from "./MainLayout.module.sass"
 
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Sidebar from "../components/Sidebar";
 import LanguageSwitcher from "../components/LanguageSwitcher";
+import { useContext, useEffect } from "react";
+import { IdentityContext } from "../context/IdentityContext";
 
 const MainLayout = () =>  {
+    const identity = useContext(IdentityContext)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (identity.currentUser == undefined) {
+            navigate("/login")
+        }
+    }, [identity.currentUser, navigate])
+    console.log("render")
+
     return (
         <div className={style.parent}>
             <div className={style.sidebarContainer}>
