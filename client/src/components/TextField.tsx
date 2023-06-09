bf607648039677827336555d225e2fc8b663b809
Stay <stay@zaroz.es>
2023-06-09 17:17:00 +0200

    Added value

--- client/src/components/TextField.tsx
+++ client/src/components/TextField.tsx
@@ -6,13 +6,14 @@ type TextFieldProps = {
     placeholder?: string,
     onChange?: (text: string) => void
     className?: string,
-    type?: "text" | "number"
+    type?: "text" | "number",
+    value?: string
 };
 
 
-const TextField: React.FC<TextFieldProps> = ({type = "text", placeholder, onChange, className}) => {
+const TextField: React.FC<TextFieldProps> = ({type = "text", value = "", placeholder, onChange, className}) => {
     return (
-        <input type={type} placeholder={placeholder} className={`${style.input} ${className}`} onChange={(e) => {
+        <input type={type} placeholder={placeholder} value={value} className={`${style.input} ${className}`} onChange={(e) => {
             if (onChange == null) return;
             onChange(e.target.value);
         }}/>
