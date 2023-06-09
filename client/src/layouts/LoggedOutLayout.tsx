ccbc3c1d4b20313a94aab71cd5705cd2d683dc9e
Stay <stay@zaroz.es>
2023-06-09 16:15:54 +0200

    Logged out layout

---
+++ client/src/layouts/LoggedOutLayout.tsx
@@ -0,0 +1,28 @@
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router"
+import { IdentityContext } from "../context/IdentityContext";
+
+import style from "./LoggedOutLayout.module.sass"
+
+const LoggedOutLayout = () =>  {
+    const identity = useContext(IdentityContext)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (identity.currentUser != undefined) {
+            navigate("/")
+        }
+    }, [identity.currentUser, navigate])
+
+    if (identity.currentUser != undefined) return (<label>redirecting</label>)
+    
+    return (
+        <div className={style.parent}>
+            <div className={style.container}>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default LoggedOutLayout;
