b5a138c8566c8484a5071bf3102e71a3f8ada5d7
Stay <stay@zaroz.es>
2023-06-09 18:41:37 +0200

    Cors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,17 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddMvcCore(x =>
 {
-    x.ModelBinderProviders.Insert(0, new FromSecurityAttribute());
+    x.ModelBinderProviders.Add(new FromSecurityAttribute());
 });
 builder.Services.AddNpgsql<LHPDatabaseContext>(builder.Configuration["Postgres"] ?? Environment.GetEnvironmentVariable("Postgres"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LHP", policy =>
+    {
+        policy.AllowAnyMethod();
+        policy.SetIsOriginAllowed(_ => true);
+        policy.AllowAnyHeader();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-{
+{ 
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<LHPDatabaseContext>();
     await dbContext.Database.MigrateAsync();
 }
+
+app.UseCors("LHP");
 
 app.MapControllers();
 
