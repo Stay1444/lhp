7a9afe5c9a0541059572232c01785459c8e3c454
Stay <stay@zaroz.es>
2023-06-13 09:48:32 +0200

    Remove items from queue once processed

--- Backend/Services/DnsService.cs
+++ Backend/Services/DnsService.cs
@@ -137,6 +137,7 @@ public sealed class DnsService : BackgroundService
                     await _pfSenseClient.HostOverrideApply();
                 }
             }
+            _updates.Clear();
         }
         finally
         {
